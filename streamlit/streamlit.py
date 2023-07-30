f5a6888e9a9aeac9665a9ad12355686a538e9a30
Andika Kristianto <75083826+rampagezero@users.noreply.github.com>
2023-07-30 20:47:31 +0700

    Update streamlit.py

--- streamlit/streamlit.py
+++ streamlit/streamlit.py
@@ -147,8 +147,8 @@ with col_kedua:
 with col_ketiga:
   data_akurasi_twitter=pd.read_csv('streamlit/Akurasi Analisis Twitter.csv',sep=';')
   data_akurasi_algoritma=pd.read_csv('streamlit/Akurasi Komparasi Algoritma.csv',sep=';')
-  fig_akurasi_twitter=px.line(data_akurasi_twitter)
-  fig_akurasi_algoritma=px.line(data_akurasi_algoritma)
+  fig_akurasi_twitter=px.line(data_akurasi_twitter,x=data_akurasi_twitter['model'],y=data_akurasi_twitter['akurasi'])
+  fig_akurasi_algoritma=px.line(data_akurasi_algoritma,x=data_akurasi_algoritma['model'],y=data_akurasi_algoritma['akurasi'])
   st.plotly_chart(fig_akurasi_twitter)
   st.plotly_chart(fig_akurasi_algoritma)
       
