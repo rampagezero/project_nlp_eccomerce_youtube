3c488543f0fc07c34305557ac090e6a6dabc6ae2
Andika Kristianto <75083826+rampagezero@users.noreply.github.com>
2023-08-27 17:14:21 +0700

    Update streamlit.py

--- streamlit/streamlit.py
+++ streamlit/streamlit.py
@@ -161,7 +161,7 @@ with col_ketiga:
   fig_akurasi_algoritma=px.line(data_akurasi_algoritma,x=data_akurasi_algoritma['model'],y=data_akurasi_algoritma['akurasi'],markers=True)
   fig_akurasi_algoritma.update_layout(title='Accuracy algorithm comparison on buyer comments on Tokopedia,buka lapak,shopee <br> and lazada accounts e-commerce',xaxis_title="Model", yaxis_title="Accuracy")
   df_proporsi_komen=pd.read_csv('Review.csv',sep=';')
-  fig_proporsi_komen=px.bar(data_ecommerce,x='Source',y='Review',color='Sentimen',barmode='group',color_discrete_map={'Negativ':'Red','Postiv':'Green'})
+  fig_proporsi_komen=px.bar(df_proporsi_komen,x='Source',y='Review',color='Sentimen',barmode='group',color_discrete_map={'Negativ':'Red','Postiv':'Green'})
   fig_proporsi_komen.update_layout(title='Comment Sentiment Proportion')
   with st.container():
     st.plotly_chart(fig_akurasi_twitter)
